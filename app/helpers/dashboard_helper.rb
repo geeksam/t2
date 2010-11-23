5a84d4f2e9ae49fa4c8141ab87c22a6ba1bab314
Sam Livingston-Gray <sam@renewfund.com>
2010-11-23 10:57:17 -0800

    refactor dashboard helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,21 +1,31 @@
 module DashboardHelper
+  def update_panel(page, panel_name, options_for_replace_html = {})
+    options_for_replace_html.reverse_merge(:partial => panel_name)
+    page.replace_html panel_name, options_for_replace_html
+    page.visual_effect :highlight, panel_name
+  end
+
+  def update_time_summaries(page)
+    update_panel page, 'time_summaries',
+      :object => @time_summaries,
+      :locals => { :client => @client }
+  end
+
   def update_currently_working_on(page)
     # Note that this is destructive of edits made in the "Currently Working On" panel.
-    page.replace_html 'currently_working_on',
-                      :partial => 'currently_working_on',
-                      :locals  => { :current_tb => TimeBlock.current }
-    page.visual_effect :highlight, 'currently_working_on'
+    update_panel page, 'currently_working_on',
+      :locals => { :current_tb => TimeBlock.current }
   end
+
   def display_edit_ttbs_form(page)
-    page.replace_html 'todays_time_blocks',
-                      :partial => 'todays_time_blocks_edit',
-                      :locals  => { :tbs => TimeBlock.today.all }
-    page.visual_effect :highlight, 'todays_time_blocks'
+    update_panel page, 'todays_time_blocks',
+      :partial => 'todays_time_blocks_edit',
+      :locals  => { :tbs => TimeBlock.today.all }
   end
+
   def display_show_ttbs_form(page)
-    page.replace_html 'todays_time_blocks',
-                      :partial => 'todays_time_blocks_show',
-                      :locals  => { :tbs => TimeBlock.today.all }
-    page.visual_effect :highlight, 'todays_time_blocks'
+    update_panel page, 'todays_time_blocks',
+      :partial => 'todays_time_blocks_show',
+      :locals  => { :tbs => TimeBlock.today.all }
   end
 end
